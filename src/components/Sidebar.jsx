703d3115170f418b7cad699a82d148eaef707781
SaqibWaheed135 <xaqibwaheed333@gmail.com>
2025-05-27 20:20:55 +0500

    profile pic added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,21 @@
-import { Home, ShoppingCart, Users, Settings, Moon } from 'lucide-react';
+import { Home, ShoppingCart, Users, Settings, Moon, Banknote } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import Logo from '../assets/eanrkarlogo.png'
 
 export default function Sidebar() {
     return (
         <div className="sidebar">
-            <h2>Flup</h2>
+
+            <img
+                src={Logo}
+                alt="EarnKar Logo"
+                style={{ width: '100px', height: '70px', margin:20, marginLeft:40, borderRadius:10}}
+            />
+
             <nav>
                 <ul>
                     <li><Home size={18} /> Dashboard</li>
-                    <li><Link to="/withdraw"><ShoppingCart size={18} />Withdraw</Link></li>
+                    <li><Link to="/withdraw"><Banknote size={18} />Withdraw</Link></li>
                     <li><Users size={18} /> Customers</li>
                     <li><Settings size={18} /> Settings</li>
                 </ul>
